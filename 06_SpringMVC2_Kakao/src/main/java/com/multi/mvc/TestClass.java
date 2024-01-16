045a77107084f6c34c1fb4b54577fdd2aabd0780
norep <norep@LAPTOP-UT95RHIL>
2024-01-16 11:16:39 +0900

    TestClass 변경 / 2024-01-16 / USER2 ■ 변경내용  - testClass 변경  - if문 추가

--- 06_SpringMVC2_Kakao/src/main/java/com/multi/mvc/TestClass.java
+++ 06_SpringMVC2_Kakao/src/main/java/com/multi/mvc/TestClass.java
@@ -2,8 +2,12 @@ package com.multi.mvc;
 
 public class TestClass {
 	
-	void test1() {
-		System.out.println("변경된 파일, versiosn 1");
+	void test1(String name) {
+		System.out.println("User2의 변경사항, versiosn 2");
+		
+		if(name.equals("홍길동")) {
+			System.out.println("홍길동입니다.");
+		}
 	}
 
 }
