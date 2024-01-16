7067e534d73dd85101dab0e43fefc01b7fa58a6e
norep <norep@LAPTOP-UT95RHIL>
2024-01-16 13:09:33 +0900

    test6 작성

--- 06_SpringMVC2_Kakao/src/main/java/com/multi/mvc/TestClass.java
+++ 06_SpringMVC2_Kakao/src/main/java/com/multi/mvc/TestClass.java
@@ -44,4 +44,12 @@ public class TestClass {
 			System.out.println("안녕?");
 		}
 	}
+	
+	void test6(String name) {
+		System.out.println("User2의 변경사항, mastrt 작성 versiosn 6");
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.println("안녕?");
+		}
+	}
 }
